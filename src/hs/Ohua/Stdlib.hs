536598012160dbb391670966209a732178a04d9c
Justus Adam <dev@justus.science>
2018-10-12 17:58:58 +0200

    [#13] Added stlib embedding
    Conditionally embeds the standard library in the compiler.
    Compile time flag can be used to turn embedding off.
    Embedding needs the ML parser, not having the ML parser enabled will throw a
    custom type error in `Ohua.Stdlib`.

---
+++ src/hs/Ohua/Stdlib.hs
@@ -0,0 +1,37 @@
+{-# LANGUAGE CPP, TemplateHaskell, DataKinds #-}
+module Ohua.Stdlib (stdlib) where
+
+import Ohua.Prelude
+
+import GHC.TypeLits as TL
+import qualified Data.ByteString.Lazy as LB (readFile)
+import qualified Language.Haskell.TH.Syntax as TH (lift)
+
+import Ohua.ALang.Lang
+import Ohua.ALang.NS
+import Ohua.Standalone
+
+#if WITH_ML_PARSER
+import Ohua.Compat.ML.Parser (parseMod)
+#endif
+
+
+#if WITH_STDLIB
+#if WITH_ML_PARSER
+stdlib :: Namespace Expression
+stdlib =
+    $(let ifacem = mempty -- TODO check this is correct
+       in liftIO (LB.readFile "src/ohuac/ohua/std.ohuaml") >>=
+          either (fail . toString) pure . resolveNS ifacem . parseMod >>=
+          TH.lift)
+#else
+stdlib ::
+       TypeError (TL.Text "The ML parser must be enabled to use the standard library.")
+    => Namespace Expression
+stdlib = error "impossible"
+
+#endif
+#else
+stdlib :: Namespace Expression
+stdlib = emptyNamespace ["ohua", "std"]
+#endif
